b9ebd2aa1d588dc94f114db1aae3c5a51c05ca91
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2022-07-11 16:27:53 +0900

    なぜ

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,41 +1,106 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include <random>
 
 using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {
-	delete model_;
-	delete sprite_;
-	delete debugcamera_;
-}
+GameScene::~GameScene() { delete model_; }
 
 void GameScene::Initialize() {
-	model_ = Model::Create();
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-
-	textureHandle_ = TextureManager::Load("cube//cube.jpg");
-
-	worldTransform_.scale_ = {3.0f, 3.0f, 3.0f};
-	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
-	worldTransform_.translation_ = {0.0f, 0.0f, 0.0f};
-
-	worldTransform_.Initialize();
-	viewProjection_.Initialize();
-
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
 
-	debugcamera_ = new DebugCamera(1280,720);
+	textureHandle_ = TextureManager::Load("mario.jpg");
 
+	model_ = Model::Create();
+
+	std::random_device seed_gen;
+
+	std::mt19937_64 engine(seed_gen());
+
+	std::uniform_real_distribution<float> rotDist(0.0f, XM_2PI);
+
+	std::uniform_real_distribution<float> posDist(-10.0f, 10.0f);
+
+	for (size_t i = 0; i < _countof(worldTransform_); i++) {
+		worldTransform_[i].scale_ = {1.0f, 1.0f, 1.0f};
+		worldTransform_[i].rotation_ = {rotDist(engine), rotDist(engine), rotDist(engine)};
+		worldTransform_[i].translation_ = {posDist(engine), posDist(engine), posDist(engine)};
+
+		//ワールドトランスフォーム
+		worldTransform_[i].Initialize();
+	}
+
+	//カメラ支店
+	viewProjection_.eye = {0, 0, -10};
+
+	viewProjection_.target = {10, 0, 0};
+
+	//上方向
+	viewProjection_.up = {cosf(XM_PI / 4.0f), sinf(XM_PI / 4.0f),0.0f};
+
+	//ビュープロじぇくション
+	viewProjection_.Initialize();
 }
 
 void GameScene::Update() {
-	debugcamera_->Update();
+
+	//視点の移動ベクトル
+	XMFLOAT3 move = {0, 0, 0};
+
+	//視点の移動速度
+	const float kEyeSpeed = 0.2f;
+
+	//押した方向のベクトル
+	if (input_->PushKey(DIK_W)) {
+		move = {0, 0, kEyeSpeed};
+	} else if (input_->PushKey(DIK_S)) {
+		move = {0, 0, -kEyeSpeed};
+	}
+
+	//視点移動
+	viewProjection_.eye.x += move.x;
+	viewProjection_.eye.y += move.y;
+	viewProjection_.eye.z += move.z;
+
+	//XMFLOAT3 move = {0, 0, 0};
+
+	const float kTargetSpeed = 0.2f;
+
+	if (input_->PushKey(DIK_LEFT)) {
+		move = {-kTargetSpeed, 0, 0};
+	} else if (input_->PushKey(DIK_RIGHT)) {
+		move = {kTargetSpeed, 0, 0};
+	}
+
+	//視点移動
+	viewProjection_.target.x += move.x;
+	viewProjection_.target.y += move.y;
+	viewProjection_.target.z += move.z;
+
+	//上方向回転処理
+
+	//上方向回転速度
+	const float kUpRotSpeed = 0.05f;
+
+	if (input_->PushKey(DIK_SPACE))
+	{
+		viewAngle += kUpRotSpeed;
+
+		//2π超えたら0にする
+		viewAngle = fmodf(viewAngle,XM_2PI);
+	}
+
+	//上方向ベクトル計算
+	viewProjection_.up = {cosf(viewAngle), sinf(viewAngle),0.0f};
+
+	//行列の再計算
+	viewProjection_.UpdateMatrix();
 }
 
 void GameScene::Draw() {
@@ -63,30 +128,48 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
-	model_->Draw(worldTransform_, debugcamera_->GetViewProjection(), textureHandle_);
+	for (size_t i = 0; i < _countof(worldTransform_); i++) {
+		model_->Draw(worldTransform_[i], viewProjection_, textureHandle_);
+	}
 	/// </summary>
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
 
-#pragma region 前景スプライト描画p
+#pragma region 前景スプライト描画
 	// 前景スプライト描画前処理
 	Sprite::PreDraw(commandList);
 
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
-	sprite_->Draw();
 	/// </summary>
 
-	////でバック
-	/*debugText_->SetPos(50, 70);
+	//でバック
+	debugText_->SetPos(50, 70);
 	debugText_->Printf(
-	  "eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);*/
+	  "eye:(%f,%f,%f)", 
+		viewProjection_.eye.x, 
+		viewProjection_.eye.y, 
+		viewProjection_.eye.z);
+
+	debugText_->SetPos(50, 90);
+	debugText_->Printf(
+	  "target:(%f,%f,%f)", 
+		viewProjection_.target.x, 
+		viewProjection_.target.y,
+		viewProjection_.target.z);
+
+	debugText_->SetPos(50, 110);
+	debugText_->Printf(
+	  "up:(%f,%f,%f)", 
+		viewProjection_.up.x,
+		viewProjection_.up.y,
+	  viewProjection_.up.z);
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
-	//
+
 	// スプライト描画後処理
 	Sprite::PostDraw();
 
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -1,17 +1,15 @@
 ﻿#pragma once
 
 #include "Audio.h"
-#include "DebugText.h"
 #include "DirectXCommon.h"
+#include "DebugText.h"
 #include "Input.h"
 #include "Model.h"
 #include "SafeDelete.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include "DebugCamera.h"
 #include <DirectXMath.h>
-using namespace DirectX;
 
 /// <summary>
 /// ゲームシーン
@@ -49,25 +47,20 @@ class GameScene {
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
+	
+	uint32_t textureHandle_ = 0;
 
 	Model* model_ = nullptr;
 
-	//スプライト
-	Sprite* sprite_ = nullptr;
-
-	WorldTransform worldTransform_;
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_[100];
+	//ビュープロじぇくション
 	ViewProjection viewProjection_;
 
-	//テクスチャハンドル
-	uint32_t textureHandle_ = 0;
+	uint32_t value_ = 0;
 
-	//サウンド
-	uint32_t soundDateHandle_ = 0;
-
-	//音声再生
-	uint32_t voiceHandle_ = 0;
-
-	DebugCamera* debugcamera_ = nullptr;
+	//カメラ上方向の角度
+	float viewAngle = 0.0f;
 
 	/// <summary>
 	/// ゲームシーン用
