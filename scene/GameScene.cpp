46125214265b1abe47b733978663cf21b96b8501
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2022-08-01 17:42:59 +0900

    tumi

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,22 +1,136 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include "PrimitiveDrawer.h"
+
+#define PI = 3.1415;
 
 using namespace DirectX;
 
 GameScene::GameScene() {}
 
 GameScene::~GameScene() {
+	delete model_;
+//	delete sprite_;
+	delete debugcamera;
+}
+
+bool GameScene::dires(Ray ray, Sphere sphere) {
+
+	Vector3 tempV = sphere.position - ray.position;
+
+	Vector3 n;
+	//= ray.direction;
+
+	n.x = ray.direction.x;
+	n.y = ray.direction.y;
+	n.z = ray.direction.z;
+
+	n.normalize();
+
+	float tempL = tempV.dot(n);
+
+	n *= tempL;
+
+	Vector3 tempPos = ray.position + n;
+
+	Vector3 a = sphere.position - tempPos;
+
+	return a.length() < sphere.r;
 }
 
 void GameScene::Initialize() {
+
+	textureHandle_ = TextureManager::Load("Resources//mario.jpg");
+
+	debugcamera = new DebugCamera(1280, 720);
+	model_ = Model::Create();
+	//sprite_ = Sprite::Create(textureHandle_, {100, 50});
+
+	//移動する物体
+	worldTransform_.scale_ = {1.0f, 1.0f, 15.0f};
+	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
+	worldTransform_.translation_ = {0.0f, 0.0f, 0.0f};
+
+	//表示される物体
+	worldTransformA_.scale_ = {5.0f, 5.0f, 5.0f};
+	worldTransformA_.translation_ = {-10.0f, 0.0f, 0.0f};
+
+	//判定取る物体
+	worldTransformK_.scale_ = {3.0f, 3.0f, 3.0f};
+	worldTransformK_.translation_ = {0.0f, 0.0f, 30.0f};
+
+	//初期化
+	worldTransform_.Initialize();
+	worldTransformK_.Initialize();
+	worldTransformA_.Initialize();
+
+	viewProjection_.Initialize();
+
+	ray.direction = {100, 0, 0};
+
+	ray.position.x = worldTransform_.translation_.x;
+	ray.position.y = worldTransform_.translation_.y;
+	ray.position.z = worldTransform_.translation_.z;
+
+	sphere.position.x = worldTransformK_.translation_.x;
+	sphere.position.y = worldTransformK_.translation_.y;
+	sphere.position.z = worldTransformK_.translation_.z;
+
+	sphere.r = 3.0f;
+
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	//物体の移動ベクトル
+	direction = {0, 0, 0};
+
+	viewProjection_.eye.y = 10.0f;
 }
 
 void GameScene::Update() {
+	debugcamera->Update();
+
+	//正面への移動速度
+	const float kTSpeed = 0.3f;
+
+	//バイオ移動
+	//横回転
+	if (input_->PushKey(DIK_W)) {
+		ray.position.y += kTSpeed;
+	} else if (input_->PushKey(DIK_S)) {
+		ray.position.y -= kTSpeed;
+	}
+
+	//正面に進む
+	if (input_->PushKey(DIK_D)) {
+		ray.position.x += kTSpeed;
+	} else if (input_->PushKey(DIK_A)) {
+		ray.position.x -= kTSpeed;
+	}
+
+	if (input_->PushKey(DIK_E)) {
+		ray.position.z += kTSpeed;
+	} else if (input_->PushKey(DIK_Q)) {
+		ray.position.z -= kTSpeed;
+	}
+
+	worldTransform_.translation_.x = ray.position.x;
+	worldTransform_.translation_.y = ray.position.y;
+	worldTransform_.translation_.z = ray.position.z;
+
+	f = false;
+
+	//レイの当たり判定
+	if (dires(ray, sphere)) {
+		f = true;
+	}
+
+	//行列の再計算
+	viewProjection_.UpdateMatrix();
+	worldTransform_.UpdateMatrix();
 }
 
 void GameScene::Draw() {
@@ -44,6 +158,17 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
+	model_->Draw(worldTransform_, debugcamera->GetViewProjection(), textureHandle_);
+	// model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+
+	// model_->Draw(worldTransformK_, viewProjection_, textureHandle_);
+	model_->Draw(worldTransformK_, debugcamera->GetViewProjection(), textureHandle_);
+
+	if (f) {
+		model_->Draw(worldTransformA_, viewProjection_, textureHandle_);
+	}
+
+	// model_->Draw(worldTransform_, debugcamera->GetViewProjection(), textureHandle_);
 	/// </summary>
 
 	// 3Dオブジェクト描画後処理
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -9,7 +9,19 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "Vector3.h"
+#include "DebugCamera.h"
 #include <DirectXMath.h>
+
+typedef struct Ray {
+	Vector3 position;
+	Vector3 direction;
+};
+
+typedef struct Sphere {
+	Vector3 position;
+	float r;
+};
 
 /// <summary>
 /// ゲームシーン
@@ -42,12 +54,42 @@ class GameScene {
 	/// </summary>
 	void Draw();
 
+	bool dires(Ray ray, Sphere sphere);
+
   private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
-	
+	DebugCamera* debugcamera = nullptr;
+
+	Model* model_ = nullptr;
+
+	WorldTransform worldTransform_;
+
+	WorldTransform worldTransformK_;
+
+	WorldTransform worldTransformA_;
+
+	ViewProjection viewProjection_;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	//サウンド
+	uint32_t soundDateHandle_ = 0;
+
+	//音声再生
+	uint32_t voiceHandle_ = 0;
+
+	uint32_t value_ = 0;
+
+	Vector3 direction;
+
+	bool f = false;
+
+	Ray ray;
+	Sphere sphere;
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
